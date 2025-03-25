d681da0138231b8a07351f7510d80c7efbf59ca1
yuki-takagi-66 <yuki.takagi@tier4.jp>
2025-03-25 20:44:37 +0900

    add test
    Signed-off-by: yuki-takagi-66 <yuki.takagi@tier4.jp>

--- control/autoware_control_validator/src/control_validator.cpp
+++ control/autoware_control_validator/src/control_validator.cpp
@@ -44,14 +44,17 @@ void AccelerationValidator::validate(
 
   res.desired_acc = desired_acc_lpf.getValue().value();
   res.measured_acc = measured_acc_lpf.getValue().value();
-  res.is_valid_acc = [this]() {
-    const double des = desired_acc_lpf.getValue().value();
-    const double mes = measured_acc_lpf.getValue().value();
-    const int8_t des_sign = std::signbit(des) ? 1 : -1;
+  res.is_valid_acc = is_in_error_range();
+}
 
-    return mes <= des * (1 + des_sign * e_scale) + e_offset &&
-           mes >= des * (1 - des_sign * e_scale) + e_offset;
-  }();
+bool AccelerationValidator::is_in_error_range() const
+{
+  const double des = desired_acc_lpf.getValue().value();
+  const double mes = measured_acc_lpf.getValue().value();
+  const int8_t des_sign = std::signbit(des) ? 1 : -1;
+
+  return mes <= des * (1 + des_sign * e_scale) + e_offset &&
+         mes >= des * (1 - des_sign * e_scale) - e_offset;
 }
 
 ControlValidator::ControlValidator(const rclcpp::NodeOptions & options)
